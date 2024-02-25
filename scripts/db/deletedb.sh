2f06df7d9a552af16066297e516f603b8eac57c1
Dom Allan <domalan8@gmail.com>
2024-02-25 11:44:06 +1100

    feat: add postgres docker and initial db

---
+++ scripts/db/deletedb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+docker compose up -d
+
+docker exec -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres -it cv-db-1 \
+psql -e -v ON_ERROR_STOP=1 -h localhost -p 5432 -U "$POSTGRES_USER" --dbname "$POSTGRES_DB"  -f /scripts/db/remove.sql
